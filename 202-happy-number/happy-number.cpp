232c617ec2d79b5f27366a851591e36ca7b4a998
Aditya Bisht <135111127+Adityabisht794@users.noreply.github.com>
2026-06-23 16:25:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (97.09%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int helper(int n){
+    int sum=0;
+    while(n>0){
+        int d=n%10;
+        n=n/10;
+        sum=sum+d*d;
+    }
+    return sum;
+}
+    bool isHappy(int n) {
+        int slow=n,fast=n;
+        while(fast!=1){
+            slow=helper(slow);
+            fast=helper(fast);
+            fast=helper(fast);
+            if(slow==fast && slow!=1){
+                return false;
+            }
+        }
+        return true;
+    }
+};

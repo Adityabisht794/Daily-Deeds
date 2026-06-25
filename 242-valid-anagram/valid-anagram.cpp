e84c0acb753b8e6979bd72181529bf9c8c916b91
Aditya Bisht <135111127+Adityabisht794@users.noreply.github.com>
2026-06-25 23:17:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (49.41%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>mp;
+        for(char i:s) mp[i]++;
+        for(char i:t) mp[i]--;
+        for( auto i :mp)
+        {
+            if(i.second) return false;
+        }
+        return true;
+    }
+};

63f9a3cf80ffb6fe65f49f85ec85ecb3459ad61f
Aditya Bisht <135111127+Adityabisht794@users.noreply.github.com>
2026-06-27 11:50:33 +0530

    Time: 23 ms (16.38%) | Memory: 22.1 MB (5.27%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
 
-    void putzero(int i,int j ,vector<vector<int>>&ans)
+    void putzero(int i,int j ,vector<vector<int>>&v, int m,int n)
     {
-        int m=ans.size();
-        int n=ans[0].size();
         for(int k=0;k<m;k++)
         {
-            ans[k][j]=0;
+            v[k][j]=0;
         }
         for(int k=0;k<n;k++)
         {
-            ans[i][k]=0;
+            v[i][k]=0;
         }
     }
 
     void setZeroes(vector<vector<int>>& v) {
         int m=v.size();
         int n=v[0].size();
-        vector<vector<int>>ans=v;
+        vector<pair<int,int>>ans;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(v[i][j]==0)
                 {
-                    putzero(i,j,ans);
+                    ans.push_back({i,j});
                 }
             }
         }
-        v=ans;
+        for(auto i : ans)
+        {
+            putzero(i.first,i.second,v,m,n);
+        }
+
     }
 };

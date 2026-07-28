4622754c06d0c4c49bceb9b43d8479dc5be3d437
Aditya Bisht <135111127+Adityabisht794@users.noreply.github.com>
2026-07-29 00:09:37 +0530

    Time: 15 ms (31.67%), Space: 16.8 MB (55.38%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minDistance(string w1, string w2) {
+        int a=w1.size();
+        int b=w2.size();
+        vector<vector<int>>dp(a+1,vector<int>(b+1,0));
+        for(int i=a-1;i>=0;i--)
+        {
+            for(int j=b-1;j>=0;j--)
+            {
+                if(w1[i]==w2[j]) 
+                {
+                    dp[i][j]=1+dp[i+1][j+1];
+                }
+                else
+                {
+                    dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        return a+b-2*dp[0][0];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

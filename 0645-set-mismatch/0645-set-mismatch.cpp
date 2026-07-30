5ea417b4d5d4bafeea600271acd5fab4ae568157
Aditya Bisht <135111127+Adityabisht794@users.noreply.github.com>
2026-07-30 11:35:39 +0530

    Time: 4 ms (40.58%), Space: 26.3 MB (37.41%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    vector<int> findErrorNums(vector<int>& v) {
-
-        unordered_map<int,int> mp;
-
-        for(int i=0;i<v.size();i++)
-        {
-            mp[v[i]]++;
-            mp[i+1]--;
-        }
-
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> freq(n + 1, 0);
         vector<int> ans(2);
 
-        for(auto x:mp)
-        {
-            if(x.second==1)
-                ans[0]=x.first;     
+        for (int x : nums)
+            freq[x]++;
 
-            else if(x.second==-1)
-                ans[1]=x.first;      
+        for (int i = 1; i <= n; i++) {
+            if (freq[i] == 2)
+                ans[0] = i;
+            else if (freq[i] == 0)
+                ans[1] = i;
         }
 
         return ans;

9170ecc843c75b30ff9e70e4165e7afa03cf90ca
Aditya Bisht <135111127+Adityabisht794@users.noreply.github.com>
2026-08-06 21:08:09 +0530

    Time: 0 ms (100%), Space: 9.9 MB (97.78%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,13 +3,14 @@ public:
     int rob(vector<int>& v) {
         int n=v.size();
         if(n==1) return v[0];
-        vector<int>dp(n);
-        dp[0]=v[0];
-        dp[1]=max(v[1],v[0]);
+        int temp1=v[0];
+        int temp2=max(v[1],v[0]);
         for(int i=2;i<n;i++)
         {
-            dp[i]=max(v[i]+dp[i-2],dp[i-1]);
+            int temp=max(v[i]+temp1,temp2);
+            temp1=temp2;
+            temp2=temp;
         }
-        return dp[n-1];
+        return temp2;
     }
 };

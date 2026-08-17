18f2de7030ac531f85f28b4e450f1bb46525dadc
Aditya Bisht <135111127+Adityabisht794@users.noreply.github.com>
2026-08-17 21:29:40 +0530

    Time: 48 ms (50.08%), Space: 11 MB (55.92%) - LeetHub

---
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int getMoneyAmount(int n) {
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+
+        for (int len = 2; len <= n; len++) {
+            for (int l = 1; l + len - 1 <= n; l++) {
+                int r = l + len - 1;
+                dp[l][r] = INT_MAX;
+
+                for (int x = l; x <= r; x++) {
+                    int left = (x > l) ? dp[l][x - 1] : 0;
+                    int right = (x < r) ? dp[x + 1][r] : 0;
+
+                    int cost = x + max(left, right);
+
+                    dp[l][r] = min(dp[l][r], cost);
+                }
+            }
+        }
+
+        return dp[1][n];
+    }
+};
